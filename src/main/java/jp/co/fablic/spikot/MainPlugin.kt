b1267e874545806459682ee38baf7355cd04bd2c
nakamuuu <keita111.mpgnt700.programming@gmail.com>
2017-06-24 19:08:57 +0900

    Setup project

---
+++ src/main/java/jp/co/fablic/spikot/MainPlugin.kt
@@ -0,0 +1,9 @@
+package jp.co.fablic.spikot
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MainPlugin : JavaPlugin() {
+    override fun onEnable() {
+        logger.info("Hello world!")
+    }
+}

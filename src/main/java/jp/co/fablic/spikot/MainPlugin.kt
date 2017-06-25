80aa399802fe061f8052d67ad12134dcf16dd6de
nakamuuu <keita111.mpgnt700.programming@gmail.com>
2017-06-25 23:54:24 +0900

    Remove a displaying of IP address
    It isn't working well :(

--- src/main/java/jp/co/fablic/spikot/MainPlugin.kt
+++ src/main/java/jp/co/fablic/spikot/MainPlugin.kt
@@ -4,9 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class MainPlugin : JavaPlugin() {
     override fun onEnable() {
-        val ip = server.ip.takeUnless { it.isNullOrEmpty() } ?: "not specified"
-        SlackNotifier().postMessage("Server launched. IP: " + ip)
-
+        SlackNotifier().postMessage("Server launched.")
         server.pluginManager.registerEvents(SlackEventListener(), this)
     }
 

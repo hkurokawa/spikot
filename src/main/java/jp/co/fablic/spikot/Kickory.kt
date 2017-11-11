2d98f53fbab54249b11c58476eb465c300d26ab1
ujihisa <ujihisa@gmail.com>
2017-11-11 10:58:59 +0900

    Add an experimental Kickory listener

---
+++ src/main/java/jp/co/fablic/spikot/Kickory.kt
@@ -0,0 +1,19 @@
+package src.main.java.jp.co.fablic.spikot
+
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+
+class Kickory : Listener {
+    @EventHandler
+    fun BlockBreakEvent(event: BlockBreakEvent) {
+        val woodlog = event.getBlock()
+        when (woodlog.getType()) {
+            Material.LOG, Material.LOG_2 -> {} // nop
+            else -> return
+        }
+        // TODO
+        event.getPlayer().sendMessage("[Kickory] You cut a block of log")
+    }
+}

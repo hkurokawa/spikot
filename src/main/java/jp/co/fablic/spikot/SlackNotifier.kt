0a15697bb47e5742fa3d2f1a05a07d169ae31b38
hkurokawa <hirosh.kurokawa@gmail.com>
2017-11-11 10:18:09 +0900

    Get Slack configs from environmental variables instead of BuildConfig.

--- src/main/java/jp/co/fablic/spikot/SlackNotifier.kt
+++ src/main/java/jp/co/fablic/spikot/SlackNotifier.kt
@@ -1,6 +1,9 @@
 package jp.co.fablic.spikot
 
 import com.github.kittinunf.fuel.Fuel
+import jp.co.fablic.spikot.system.slackChannelName
+import jp.co.fablic.spikot.system.slackToken
+import jp.co.fablic.spikot.system.envVars
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.launch
 import java.util.logging.Logger
@@ -8,18 +11,24 @@ import java.util.logging.Logger
 class SlackNotifier {
     private val logger = Logger.getLogger(SlackNotifier::javaClass.name)
     private val url = "https://slack.com/api/chat.postMessage"
+    private val token by lazy {
+        envVars[slackToken]
+    }
+    private val channelName by lazy {
+        envVars[slackChannelName]
+    }
 
     fun postMessage(text: String, color: String? = null) {
         if (color == null) {
             postMessage(listOf(
-                    "token" to BuildConfig.SLACK_TOKEN,
-                    "channel" to BuildConfig.SLACK_CHANNEL_NAME,
+                    "token" to token,
+                    "channel" to channelName,
                     "text" to text
             ))
         } else {
             postMessage(listOf(
-                    "token" to BuildConfig.SLACK_TOKEN,
-                    "channel" to BuildConfig.SLACK_CHANNEL_NAME,
+                    "token" to token,
+                    "channel" to channelName,
                     "attachments" to String.format("[{\"text\": \"%s\", \"color\": \"%s\"}]", text, color)
             ))
         }
